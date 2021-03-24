373cf1dfb4e5bbe0defc0f2cbdc79d50bb0cd065
Lucinda Sisk <lucinda.sisk@gmail.com>
2021-03-24 17:59:02 -0600

    clean up repo for publication

--- B.copy_dtimasks.sh
+++ B.copy_dtimasks.sh
@@ -7,12 +7,16 @@ fp=/Volumes/group/proc/TIGERanalysis/qT1/subjDir
 adir=/Volumes/iang/TIGER_data/TIGER_DTI_Analysis/TIGER-T1/matproc
 bdir=/Volumes/iang/TIGER_data/TIGER_DTI_Analysis/TIGER-T2/matproc
 edir=/Volumes/iang/ELS_data/ELS_DTI_Analysis/ELS_T3/matproc
+cdir=/Volumes/group/proc/ELSanalysis/DTIAnalysis/ELS_T3/matproc
+ddir=/Volumes/iang/ELS_data/ELS_DTI_Analysis/ELS_T4/matproc
 
 cd $fp
 subs=$(ls)
 
-for sub in $subs; do
-  for roi in Left_Uncinate.nii.gz Right_Uncinate.nii.gz Right_Cingulum_Cingulate.nii.gz Left_Cingulum_Cingulate.nii.gz; do
+for sub in 112-T4 164-T3 176-T3 188-T3 307-T3 066-T3 072-T4 ; do
+  mkdir $fp/$sub
+  for roi in Callosum_Forceps_Major.nii.gz Callosum_Forceps_Minor.nii.gz Right_Uncinate.nii.gz Left_Uncinate.nii.gz\
+  Left_Cingulum_Cingulate.nii.gz Right_Cingulum_Cingulate.nii.gz ; do
     if [ -e $fp/${sub}/$roi ] ; then
       echo 'Already copied '$roi' for' $sub
     elif [ -e $adir/$sub/dti60trilin/ROIs/$roi ] ; then
@@ -24,6 +28,12 @@ for sub in $subs; do
     elif [ -e $edir/els$sub/dti60trilin/ROIs/$roi ] ; then
       echo 'Copying' $roi 'for' $sub
       cp $edir/els$sub/dti60trilin/ROIs/$roi $fp/${sub}/$roi
+    elif [ -e $cdir/els$sub/dti60trilin/ROIs/$roi ] ; then
+      echo 'Copying' $roi 'for' $sub
+      cp $cdir/els$sub/dti60trilin/ROIs/$roi $fp/${sub}/$roi
+    elif [ -e $ddir/els$sub/dti60trilin/ROIs/$roi ] ; then
+      echo 'Copying' $roi 'for' $sub
+      cp $ddir/els$sub/dti60trilin/ROIs/$roi $fp/${sub}/$roi
     else
       echo 'Could not find '$roi' for' $sub
       echo ${roi}': '${sub} >> '/Volumes/group/proc/TIGERanalysis/qT1/scripts/NeedsDtiMasks.txt'
